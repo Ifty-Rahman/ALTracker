2fa1ba776fd8ec19aa225e0ca21f057539e2e2a3
Ifty Rahman <Ifty09rahman@gmail.com>
2025-10-16 00:52:29 +0600

    test api calls

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://graphql.anilist.co" }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
   </StrictMode>,
 )
