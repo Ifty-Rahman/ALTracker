576fb70272f87214931639794c4c7b98d443e98e
Ifty Rahman <Ifty09rahman@gmail.com>
2025-10-23 20:44:18 +0600

    add user stats

--- src/components/UserStats.jsx
+++ src/components/UserStats.jsx
@@ -1,14 +1,47 @@
+import "../css/UserStats.css";
+
 function UserStats({ stats }) {
   return (
-    <div className="user-stats">
-      <h2>Overview</h2>
-      <p>Anime</p>
-      <p>Total Entries</p>
-      <p>{stats?.count}</p>
-      <p>Time Watched</p>
-      <p>{(stats?.minutesWatched / 60).toFixed()} hours</p>
-      <p>Mean Score</p>
-      <p>{stats?.meanScore}</p>
+    <div className="user-stats-container">
+      <h2 className="overview-title">Overview</h2>
+
+      <div className="stats-box">
+        <div className="stats-section left">
+          <p className="section-title">Anime</p>
+          <div className="stat-item">
+            <p className="stat-label">Total Entries</p>
+            <p className="stat-value">{stats?.anime.count}</p>
+          </div>
+          <div className="stat-item">
+            <p className="stat-label">Time Watched</p>
+            <p className="stat-value">
+              {(stats?.anime.minutesWatched / 60).toFixed()} hr
+            </p>
+          </div>
+          <div className="stat-item">
+            <p className="stat-label">Mean Score</p>
+            <p className="stat-value">{stats?.anime.meanScore}</p>
+          </div>
+        </div>
+
+        <div className="divider"></div>
+
+        <div className="stats-section right">
+          <p className="section-title">Manga</p>
+          <div className="stat-item">
+            <p className="stat-label">Total Entries</p>
+            <p className="stat-value">{stats?.manga.count}</p>
+          </div>
+          <div className="stat-item">
+            <p className="stat-label">Chapters Read</p>
+            <p className="stat-value">{stats?.manga.chaptersRead}</p>
+          </div>
+          <div className="stat-item">
+            <p className="stat-label">Mean Score</p>
+            <p className="stat-value">{stats?.manga.meanScore}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
