7c55363943acaefb4f103e9e2b1f5b55aff6b54a
Ifty Rahman <Ifty09rahman@gmail.com>
2025-10-23 20:44:45 +0600

    add data caching

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 import { setContext } from "@apollo/client/link/context";
 import "./css/App.css";
-import Discover from "./pages/discover";
+import Discover from "./pages/Discover";
 import UserList from "./pages/Userlist";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import ProfilePage from "./pages/profile";
+import ProfilePage from "./pages/Profile";
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("anilist_token");
@@ -51,8 +51,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Discover />} />
           <Route path="/Userlist" element={<UserList />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Profile" element={<ProfilePage />} />
         </Routes>
       </ApolloProvider>
     </main>
