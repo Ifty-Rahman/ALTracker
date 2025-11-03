b24d22b95ac10cfcc12338e6712a00e3be827425
Ifty Rahman <Ifty09rahman@gmail.com>
2025-11-03 13:25:12 +0600

    add anime or manga toggle

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import PopularThisSeason from "../components/PopularThisSeason.jsx";
 import PopularAllTime from "../components/PopularAllTime.jsx";
 import UpcomingNextSeason from "../components/Upcoming.jsx";
-import "../css/Discover.css";
 import TrendingAnime from "../components/TrendingAnime.jsx";
+import PopularManhwa from "../components/PopularManhwa.jsx";
+import "../css/Discover.css";
 
 function Discover() {
+  const [mediaType, setMediaType] = useState("ANIME");
+
   return (
     <div className="discover">
-      <TrendingAnime />
-      <PopularThisSeason />
-      <UpcomingNextSeason />
-      <PopularAllTime />
+      <div className="discover-toggle">
+        <ToggleButtonGroup
+          className="toggle-group"
+          value={mediaType}
+          exclusive
+          onChange={(e, value) => value && setMediaType(value)}
+        >
+          <ToggleButton value="ANIME" aria-label="anime">
+            Anime
+          </ToggleButton>
+          <ToggleButton value="MANGA" aria-label="manga">
+            Manga
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+
+      <TrendingAnime type={mediaType} />
+      {mediaType === "ANIME" ? (
+        <>
+          <PopularThisSeason />
+          <UpcomingNextSeason />
+        </>
+      ) : (
+        <>
+          <PopularManhwa />
+        </>
+      )}
+      <PopularAllTime type={mediaType} />
     </div>
   );
 }
